5b455056a8831258079a0f2a83c871094af8d241
yammik <mkim0514@gmail.com>
2018-09-18 09:11:59 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+
+    erb :create_puppy
+  end
+
+  post '/new' do
+    @name = params["name"]
+    @breed = params["breed"]
+    @age = params["age"]
+    Puppy.new(@name, @breed, @age)
+
+    erb :display_puppy
+  end
+
 end
--- spec/sinatra_basic_forms_lab_spec.rb
+++ spec/sinatra_basic_forms_lab_spec.rb
@@ -1,19 +1,19 @@
 describe App do
 
   describe 'GET /' do
-    
+
     it 'sends a 200 status code' do
       get '/'
       expect(last_response.status).to eq(200)
     end
 
-    it 'renders welcome' do 
+    it 'renders welcome' do
       visit '/'
       expect(page).to have_link("Click Here To List A Puppy")
     end
   end
 
-  describe 'GET /NEW' do 
+  describe 'GET /NEW' do
     it 'sends a 200 status code' do
       get '/new'
       expect(last_response.status).to eq(200)
@@ -29,7 +29,7 @@ describe App do
   end
 
   describe 'POST /' do
-    it "displays the puppy" do 
+    it "displays the puppy" do
       visit '/new'
 
       fill_in(:name, :with => "Butch")
@@ -42,5 +42,5 @@ describe App do
     end
   end
 
-  
+
 end
